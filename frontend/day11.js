c238744e19d10db485494cb19ffde55fd72b3fcf
Yogesh Awasthi <yogeshawasthi54321@gmail.com>
2025-06-11 22:06:12 +0545

    day 11

--- frontend/day11.js
+++ frontend/day11.js
@@ -1,23 +1,23 @@
-// console.log("Hello form this side")
+console.log("Hello form this side")
 
-// let boxes = document.getElementsByClassName("box")
-// console.log(boxes);
+let boxes = document.getElementsByClassName("box")
+console.log(boxes);
 
-// // third box to red
-// boxes[2].style.backgroundColor = "red"
-// boxes[3].style.backgroundColor = "red"
-// boxes[2].style.backgroundColor = "red"
+// third box to red
+boxes[2].style.backgroundColor = "red"
+boxes[3].style.backgroundColor = "red"
+boxes[2].style.backgroundColor = "red"
 
 
-// // can use to change the color of the box with id red
-// document.getElementById("red").style.backgroundColor = "green"
-// document.getElementById("yellow").style.backgroundColor = "yellow"
+// can use to change the color of the box with id red
+document.getElementById("red").style.backgroundColor = "green"
+document.getElementById("yellow").style.backgroundColor = "yellow"
 
-// document.querySelector(".box").style.backgroundColor = "pink"//frist one will be selected
+document.querySelector(".box").style.backgroundColor = "pink"//frist one will be selected
 
-document.querySelectorAll(".box").forEach((element) => {
-    element.style.backgroundColor = "blue"
-});
+// document.querySelectorAll(".box").forEach((element) => {
+//     element.style.backgroundColor = "blue"
+// });
 
 e = document.getElementsByTagName("div");
 e[4].closest("#redbox")
