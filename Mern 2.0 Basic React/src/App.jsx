f79dd4882af3adff3984886aa7ec43ef92cc28d9
Yogesh Awasthi <yogeshawasthi54321@gmail.com>
2025-06-28 21:36:57 +0545

    day 28 done

--- Mern 2.0 Basic React/src/App.jsx
+++ Mern 2.0 Basic React/src/App.jsx
@@ -5,29 +5,12 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const name = "Yogesh "
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Yogesh + Awasthi</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <h1>Yogesh {name}</h1>
+        {/* <p>{1+1}</p> */}
     </>
   )
 }
