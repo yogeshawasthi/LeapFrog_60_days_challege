56fdcafa8846fe6acd2a8e76ae18e31baba1fd16
Yogesh Awasthi <yogeshawasthi54321@gmail.com>
2025-07-03 21:44:30 +0545

    day 33 project intitalization error but learned about tialwind css

---
+++ MERN2.0_LMS/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <div className='bg-blue-500 p-2 m-4 text-cyan-400'>Hi How are you</div>
+    </div>
+  )
+}
+
+export default App
