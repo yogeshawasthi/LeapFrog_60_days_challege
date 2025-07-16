a6573e22916d8d00918dd7dd7592e2b4e1ec05ac
Yogesh Awasthi <yogeshawasthi54321@gmail.com>
2025-07-16 23:32:00 +0545

    Day 46 practical of socket.io diffreence betmin io.emit and socket.emit

--- Learning_Sockets/app.js
+++ Learning_Sockets/app.js
@@ -1,17 +1,30 @@
-const express = require("express")
-const app = express()
-const{Server} = require('socket.io')//importing socket.io
+const express = require("express");
+const app = express();
+const { Server } = require("socket.io"); // no need to import Socket
 
+const server = app.listen(4000, () => {
+    console.log("Server has started at Port 4000");
+});
 
+const io = new Server(server); // create socket.io server
 
+io.on("connection", (socket) => {
+//     socket.emit("hi",{
+//         greeting : "Helo how are you"
+//     })
+    // console.log(socket.id); // this will correctly log the connected socket's ID
+   
+    // console.log("Someone has connected!");
 
- const server = app.listen(4000,()=>{
-    console.log("Sercer Has started at Port 4000")
-})
+    socket.on('sendData',(data)=>{
+        if(data){
+           
+           io.emit("response","Thank You your data was recieved ")
+        }
+    })   
 
-
-const io = new  Server(server) //instance of socket.io
-
-io.on('connection',()=>{
-    console.log("Someone Has Connected")
-})
+    
+    // socket.on("disconnect",()=>{
+    //     console.log("Disconnected a user")
+    // })
+});
