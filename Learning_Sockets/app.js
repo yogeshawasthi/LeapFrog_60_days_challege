208aec6c60f5a9937689c32a2342989b24b47c33
Yogesh Awasthi <yogeshawasthi54321@gmail.com>
2025-07-17 22:43:57 +0545

    Day 47 DOne

--- Learning_Sockets/app.js
+++ Learning_Sockets/app.js
@@ -18,10 +18,10 @@ io.on('connection', (socket) => {
     //addBook
 
     socket.on('addBook', async (data) => {
-      try {
-         if (data) {
+        try {
+            if (data) {
                 const { bookName, bookPrice } = data
-                const newBook = await Books.create({
+                const newBook = await Book.create({
                     bookName,
                     bookPrice
                 })
@@ -30,11 +30,66 @@ io.on('connection', (socket) => {
                     status: 200, message: "Book Created Successfully",
                     data: newBook
                 })
-         }
-      } catch (error) {
-        socket.emit("response",{status : 500 , message :"Something went wrong"})
-        
-      }
-        })
+            }
+        } catch (error) {
+            socket.emit("response", { status: 500, message: "Something went wrong" })
+
+        }
+    })
+
+
+
+    // get books
+
+    socket.on("getBook", async (data) => {
+        try {
+            const books = await Book.find()
+
+            socket.emit("response", { status: 200, message: "Book Fetched ", data: books })
+
+        } catch (error) {
+            socket.emit("response", { status: 500, message: "Something Went Wrong " })
+
+        }
+    })
+
+    //update book
+    socket.on("updateBook", async (data) => {
+        try {
+            if (data) {
+                const { bookName, bookPrice, bookId } = data;
+                const updatedBook = await Book.findByIdAndUpdate(
+                    bookId,
+                    {
+                        bookName,
+                        bookPrice
+                    },
+                    {
+                        new: true
+                    }
+                );
+                if (updatedBook) {
+                    socket.emit("response", { status: 200, message: "Book Updated Successfully", data: updatedBook });
+                } else {
+                    socket.emit("response", { status: 404, message: "Book not found" });
+                }
+            }
+        } catch (error) {
+            socket.emit("response", { status: 500, message: "Something Went Wrong " });
+        }
+    })
+    //delteBook
+    socket.on("deleteBook", async (data) => {
+        try {
+            if (data) {
+                const { bookId } = data
+                await Book.findByIdAndDelete(bookId)
+                socket.emit("response", { status: 200, message: "Book Deleted " })
+            }
+        } catch (error) {
+            socket.emit("response", { status: 500, message: "Something Went Wrong " })
+
+        }
+    })
 
 })
