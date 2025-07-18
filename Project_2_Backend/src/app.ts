96f779ecc98378d59517ce466e0104e736900977
Yogesh Awasthi <yogeshawasthi54321@gmail.com>
2025-07-18 22:08:15 +0545

    .

--- Project_2_Backend/src/app.ts
+++ Project_2_Backend/src/app.ts
@@ -4,6 +4,12 @@ const PORT = 3000
 
 
 
+
 app.get("/",(req,res)=>{
     res.send("Hello, World!");
+})
+
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on http://localhost:${PORT}`);
 })
