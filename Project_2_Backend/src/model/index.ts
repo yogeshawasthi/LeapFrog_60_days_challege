62e3355de2377cec0e8e8738057f47f140eb25cf
Yogesh Awasthi <yogeshawasthi54321@gmail.com>
2025-07-19 21:46:55 +0545

    .

---
+++ Project_2_Backend/src/model/index.ts
@@ -0,0 +1,33 @@
+import { Sequelize,DataType } from "sequelize";
+import dbConfig from "../config/dbConfig";
+
+const sequelize = new Sequelize(dbConfig.db,dbConfig.user,dbConfig.password,{
+    host : dbConfig.host,
+
+    dialect : dbConfig.dialect,
+    port : 3306,
+    pool:{
+        acquire : dbConfig.pool.acquire,
+        min : dbConfig.pool.min,
+        max : dbConfig.pool.max,
+        idle : dbConfig.pool.idle        
+    }
+
+
+})
+
+sequelize
+.authenticate()
+.then(()=>{
+    console.log("Connected")
+})
+.catch((err)=>{
+    console.log(err)
+})
+
+const db:any = {}
+
+db.Sequelize = Sequelize
+db.sequelize = sequelize
+
+db.sequelize.sync({force : false})
