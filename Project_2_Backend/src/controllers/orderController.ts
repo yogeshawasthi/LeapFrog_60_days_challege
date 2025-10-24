2ce414b2c144a8b8897c90e9abb719721b2fcf61
Yogesh Awasthi <yogeshawasthi54321@gmail.com>
2025-10-24 21:57:16 +0545

    Let`s Do it

--- Project_2_Backend/src/controllers/orderController.ts
+++ Project_2_Backend/src/controllers/orderController.ts
@@ -255,3 +255,7 @@ export default new OrderController();
 // vanzi have gone to daizi i missing the noise they were creating
 // hhi how are you 
 // finally completed
+// i need to do complete overview of the project form forntend to backend 
+// form tommorw i will be doing 2 hours moring and 2 hours evening coding
+// i need to do complete overview of the project form forntend to backend 
+// form tommorw i will be doing 2 hours moring and 2 hours evening coding
